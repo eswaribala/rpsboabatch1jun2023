3172d1639936bc4d51598b6b096795133649900a
eswaribala <parameswaribala@gmail.com>
2023-06-05 12:56:26 +0530

    updated

---
+++ customerapi/src/main/java/com/boa/customerapi/models/Customer.java
@@ -0,0 +1,31 @@
+package com.boa.customerapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Inheritance(strategy = InheritanceType.JOINED)
+@Entity
+@Table(name="Customer")
+public class Customer {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name="Account_No")
+    private long accountNo;
+   @Embedded
+    private FullName name;
+    @Column(name="Email",nullable = false,length = 150)
+    private String email;
+    @Column(name="Password",nullable = false,length = 10)
+    private String password;
+    @Column(name="Contact_No")
+    private long contactNo;
+
+}
