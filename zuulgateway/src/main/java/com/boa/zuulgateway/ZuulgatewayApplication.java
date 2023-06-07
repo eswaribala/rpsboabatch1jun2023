e4b8642979c930f5a37ea728748e7784ebdc809b
eswaribala <parameswaribala@gmail.com>
2023-06-07 15:18:14 +0530

    updated

---
+++ zuulgateway/src/main/java/com/boa/zuulgateway/ZuulgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.boa.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulgatewayApplication.class, args);
+    }
+
+}
