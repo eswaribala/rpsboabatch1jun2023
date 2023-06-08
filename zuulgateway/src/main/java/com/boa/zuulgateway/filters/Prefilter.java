0c68d9142f766cc8639eee4c089172a57d7a28a1
eswaribala <parameswaribala@gmail.com>
2023-06-08 12:26:02 +0530

    updated

--- zuulgateway/src/main/java/com/boa/zuulgateway/filters/Prefilter.java
+++ zuulgateway/src/main/java/com/boa/zuulgateway/filters/Prefilter.java
@@ -1,14 +1,24 @@
 package com.boa.zuulgateway.filters;
 
 import com.boa.zuulgateway.vos.JwtRequest;
+import com.boa.zuulgateway.vos.JwtResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +28,10 @@ import java.util.stream.Collectors;
 public class Prefilter extends ZuulFilter {
     @Autowired
     private RestTemplate restTemplate;
+    private ObjectMapper objectMapper;
+
+    @Value("${authUrl}")
+    private String authUrl;
 
     @Override
     public String filterType() {
@@ -61,8 +75,59 @@ public class Prefilter extends ZuulFilter {
         jwtRequest.setUserName(data.get(0));
         jwtRequest.setUserPwd(data.get(1));
 
+        //step 2
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity request = new HttpEntity<>(jwtRequest, headers);
+            //phase 1 get jwt token
+            //synchronous inter service communication
+            //http://localhost:9093/signin
+	    /*
+	     * {
+	"userName":"eswari",
+	"userPwd":"test@123"
+}
+	     */
+       //step 3
+            ResponseEntity<?> authResponse = restTemplate.
+                    postForEntity(authUrl + "signin", request, String.class);
+            System.out.println(authResponse.getBody().toString());
+
+        }
+        catch(Exception exception)
+        {
+            System.out.println("token : {} Validation failed" + token );
+            //Do not route requests
+            ctx.setSendZuulResponse(false);
+            responseError(ctx, -403, "invalid token");
+        }
 
 
         return null;
     }
+
+
+
+    /**
+     * Response Exception Information to Front End
+     */
+    private void responseError(RequestContext ctx, int code, String message) {
+        HttpServletResponse response = ctx.getResponse();
+        JwtResponse errResult = new JwtResponse(code+"->"+message);
+
+        ctx.setResponseBody(toJsonString(errResult));
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setContentType("application/json;charset=utf-8");
+    }
+    private String toJsonString(Object o) {
+        try {
+            return objectMapper.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            System.out.println("json Serialization failed"+e);
+            return null;
+        }
+    }
+
 }
