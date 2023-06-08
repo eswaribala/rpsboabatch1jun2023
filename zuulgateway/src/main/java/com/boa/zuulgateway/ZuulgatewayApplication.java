e8c075c6254e335d603ddc5154d13ca7e79bdd3c
eswaribala <parameswaribala@gmail.com>
2023-06-08 12:12:01 +0530

    updated

--- zuulgateway/src/main/java/com/boa/zuulgateway/ZuulgatewayApplication.java
+++ zuulgateway/src/main/java/com/boa/zuulgateway/ZuulgatewayApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,7 +13,13 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ZuulgatewayApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ZuulgatewayApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 
 }
---
+++ zuulgateway/src/main/java/com/boa/zuulgateway/filters/Prefilter.java
@@ -0,0 +1,33 @@
+package com.boa.zuulgateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class Prefilter extends ZuulFilter {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
